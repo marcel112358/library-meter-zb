ec021741ef7d5243fa70d6867c29688a69748a97
marcel112358 <marcel@saaro.ch>
2020-10-12 09:36:09 +0200

    Optimize for python2 (dsm)

---
+++ watch.py
@@ -0,0 +1,12 @@
+import requests
+import re
+from datetime import datetime
+import time
+
+while 1:
+    r = requests.get('https://www.zb.uzh.ch/themes/zb/assets/js/gauge.value.js')
+    m = re.search('gauge.set\((.+?)\);', r.text)
+    if m:
+        found = m.group(1)
+        print(datetime.now().strftime("%Y/%m/%dT%H:%M:%S"), found, sep=',')
+    time.sleep(10)
